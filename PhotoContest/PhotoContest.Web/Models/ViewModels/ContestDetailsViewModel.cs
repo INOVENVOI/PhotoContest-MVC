1639048d9f0423222025b6800add0f92a1669bfc
Daniela Petrova <daniella_petrova@abv.bg>
2015-10-31 00:09:35 +0200

    Differently resize picture by displaying

--- PhotoContest/PhotoContest.Web/Models/ViewModels/ContestDetailsViewModel.cs
+++ PhotoContest/PhotoContest.Web/Models/ViewModels/ContestDetailsViewModel.cs
@@ -54,15 +54,15 @@
             }
         }
 
-        public int PageCount { get; }
-        public int TotalItemCount { get; }
-        public int PageNumber { get; }
-        public int PageSize { get; }
-        public bool HasPreviousPage { get; }
-        public bool HasNextPage { get; }
-        public bool IsFirstPage { get; }
-        public bool IsLastPage { get; }
-        public int FirstItemOnPage { get; }
-        public int LastItemOnPage { get; }
+        public int PageCount { get; set; }
+        public int TotalItemCount { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public bool HasPreviousPage { get; set; }
+        public bool HasNextPage { get; set; }
+        public bool IsFirstPage { get; set; }
+        public bool IsLastPage { get; set; }
+        public int FirstItemOnPage { get; set; }
+        public int LastItemOnPage { get; set; }
     }
 }
